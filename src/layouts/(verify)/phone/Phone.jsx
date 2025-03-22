0c1fc33b512df2c07578782f7f08a6975e31a360
toan <nguyenductoan2k4@gmail.com>
2025-03-22 20:18:40 +0700

    update vercel

--- src/layouts/(verify)/phone/Phone.jsx
+++ src/layouts/(verify)/phone/Phone.jsx
@@ -3,7 +3,7 @@ import styles from '../../(verify)/phone/VerifyPhoneForm.module.scss'
 
 function Phone() {
     return(
-        <VerifyPhone></VerifyPhone>
+        <VerifyPhone/>
     )
 }
 export default Phone;
