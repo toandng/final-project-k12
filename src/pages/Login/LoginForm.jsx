37dcd3e99ff222a5d69188f71a8d8834bfcc0c88
toan <nguyenductoan2k4@gmail.com>
2025-04-11 20:03:36 +0700

    use tabs, tab, accridion, update home, edit login, regiser = component form

--- src/pages/Login/LoginForm.jsx
+++ src/pages/Login/LoginForm.jsx
@@ -1,34 +1,34 @@
 import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
 import { useNavigate } from "react-router-dom";
 import styles from "./LoginForm.module.scss";
 import Button from "../../components/Button";
 import config from "../../config";
-import authServices from "../../services/authServices";
-import loginSchema from "../../schema/loginSchema";
 import httpRequest from "../../utils/httpRequest";
 import useLoading from "../../hooks/useLoading";
+import Form, { TextInput } from "../../components/Forms";
+import authServices from "../../services/authServices";
 
 export default function LoginForm() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(loginSchema),
-  });
-
-  const navigate = useNavigate();
   const [error, setGeneralError] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { setLoading } = useLoading();
-  const onSubmit = async (data) => {
-    setLoading(true);
-    setGeneralError(""); // Reset lỗi trước khi gửi request
+  const navigate = useNavigate();
 
+  const handleSubmit = async( data) => {
+    setLoading(true);
+    setGeneralError("");
+    const formData = {
+      email: data.email,
+      password: data.password
+    };
+
+    console.log(data);
+    
+    
     try {
-      const res = await authServices.login(data)
-      const token = res?.data?.access_token
+      const res = await authServices.login(formData)
+      const token = res.data?.access_token
       if (token) {
         console.log("wasjdh");
         
@@ -44,36 +44,52 @@ export default function LoginForm() {
         setLoading(false)
       },500)
     }
-  };
-
+  }
+  
   return (
-    <>
+    <div className={styles.container}>
       <div className={styles.img}>
         <img src="/img/7.jpg" alt="Background" />
       </div>
-      <form onSubmit={handleSubmit(onSubmit)} className={styles.container}>
+
+      <Form onSubmit={handleSubmit}>
         <div>
           <h2>Welcome to Scrap Plan</h2>
-          <p>Create an account or login to join your orders</p>
+          <p  className={styles.p}>Create an account or login to join your orders</p>
         </div>
-        <div>
-          <label className={styles.login}>Email</label>
-          <input className={styles.email} type="email" {...register("email")} />
-          {errors.email && <p className={styles.error}>{errors.email.message}</p>}
 
-          <label className={styles.login}>Password</label>
-          <input className={styles.password} type="password" {...register("password")} />
-          {errors.password && <p className={styles.error}>{errors.password.message}</p>}
-        </div>
+        <label className={styles.login}>Email</label>
+        <TextInput
+          name="email"
+          className={styles.email}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <br />
+
+        <label className={styles.login}>Password</label>
+        <TextInput
+          name="password"
+          type="password"
+          className={styles.password}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
         {error && <p className={styles.error}>{error}</p>}
+
+       <div className={styles.buttonLogin}>
         <Button size="lg" type="submit">
-          LOGIN
-        </Button>
+              LOGIN
+          </Button>
+       </div>
+
         <div className={styles.needAccount}>
           <span className={styles.span}>Don&apos;t have an account?</span>
-          <Button type="Link" to={config.routes.register}> Register here!</Button>
+          <Button  className={styles.newAccount} type="Link" to={config.routes.register}> Register here!</Button>
         </div>
-      </form>
-    </>
+      </Form>
+    </div>
   );
 }
--- src/pages/Register/RegisterForm.jsx
+++ src/pages/Register/RegisterForm.jsx
@@ -1,139 +1,62 @@
-import { useEffect, useState } from "react";
+
 import { useNavigate } from "react-router-dom";
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
 import styles from './RegisterForm.module.scss';
 import config from "../../config";
 import authServices from "../../services/authServices";
 import httpRequest from "../../utils/httpRequest";
 import Button from "../../components/Button";
-import registerSchema from "../../schema/registerSchema";
-import useDebounce from "../../hooks/useDebounce";
+import registerSchema from "../../schema/registerSchema"
+
 import useLoading from "../../hooks/useLoading";
+import Form,{ TextInput } from "../../components/Forms";
 
 export default function RegisterForm() {
-  const {
-    register, 
-    handleSubmit,
-    watch,
-    trigger,
-    setError,
-    formState: { errors }
-  } = useForm({
-    resolver: yupResolver(registerSchema),
-  });
 
-  const [error, setGeneralError] = useState('');
+
 
   const { setLoading } = useLoading();
   // Move this before emailValue
-  const emailValue = watch("email");
-  const debounceValue = useDebounce(emailValue, 800);
-
   const navigate = useNavigate();
 
-  const onSubmit = async (data) => {
-    setLoading(true);
-    setGeneralError('');
+  const onSubmit = async (userInfo) => {
+    setLoading(true)
+    console.log(userInfo);
+    const data = await authServices.register(userInfo);
 
-    const requestData = {
-      firstName: data.firstName,
-      lastName: data.lastName,
-      email: data.email,
-      password: data.password,
-      password_confirmation: data.password_confirmation,
-    };
-
-    try {
-      const response = await authServices.register(requestData);
-      if (response.status === 'success') {
-        httpRequest.setToken(response.access_token);
-        navigate(config.routes.verifyPhone);
-      } else {
-        setGeneralError(response.message?.general || 'Đăng ký thất bại');
-      }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setTimeout(() =>{
-        setLoading(false)
-      },500)
+    if (data.status === "success") {
+      httpRequest.setToken(data.access_token);
+      navigate(config.routes.verifyPhone);
     }
   };
 
-  useEffect(() => {
-    if (!debounceValue) return;
-
-    (async () => {
-      const isValid = await trigger("email");
-      if (isValid) {
-        console.log("Kiểm tra email:", debounceValue);
-        const exists = await authServices.checkEmail(debounceValue);
-        console.log("Kết quả check email:", exists);
-        if (exists) {
-          setError("email", {
-            type: "manual",
-            message: "Email này đã tồn tại..."
-          });
-        }
-      }
-    })();
-  }, [debounceValue, trigger, setError]);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className={`${styles.container}`}>
-      <h2>Register</h2>
-
-      <p>First Name</p>
-      <input
-        className={`${styles.firstName}`}
-        type="text"
-        {...register('firstName')}
-        required
-      />
-      {errors.firstName && <p>{errors.firstName.message}</p>}
-
+    <div className={styles.container}>
+    <h1>Register</h1>
+    <Form
+      schema={registerSchema}
+      formProps={{ mode: "onChange" }}
+      onSubmit={onSubmit}
+    >
+       <p>First Name</p>
+      <TextInput name="firstName" placeholder="First name..."  className={styles.firstName}/>
       <p>Last Name</p>
-      <input
-        className={`${styles.lastName}`}
-        type="text"
-        name="lastName"
-        placeholder="Last Name"
-        {...register('lastName')}
-      />
-      {errors.lastName && <p>{errors.lastName.message}</p>}
-
+      <TextInput name="lastName" placeholder="Last name..." />
       <p>Email</p>
-      <input
-        className={`${styles.email}`}
-        type="email"
-        name="email"
-        {...register('email')}
-      />
-      {errors.email && <p>{errors.email.message}</p>}
+      <TextInput name="email" placeholder="Email..." />
 
       <p>Password</p>
-      <input
-        className={`${styles.password}`}
-        type="password"
-        name="password"
-        {...register('password')}
-      />
-      {errors.password && <p>{errors.password.message}</p>}
-
+      <TextInput name="password" placeholder="Password..."  type="password" />
       <p>Confirm Password</p>
-      <input
-        className={`${styles.confirmPassword}`}
+      <TextInput
+        name="password_confirmation"
+        placeholder="Password confirmation..."
         type="password"
-        {...register('password_confirmation')}
       />
-      {errors.password_confirmation && <p>{errors.password_confirmation.message}</p>}
+      <Button  className={styles.buttonRegster}   size="lg" type="submit">Register</Button>
+    </Form>
 
-      {error && <p className={styles.error}>{error}</p>}
-
-      <Button size="lg" type="submit">
-        REGISTER
-      </Button>
-    </form>
+   
+  </div>
   );
 }
