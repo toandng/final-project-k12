5906046c82493366e8cddce510928baf91345f5c
toan <nguyenductoan2k4@gmail.com>
2025-04-21 11:13:13 +0700

    update ProctectRoute

--- src/components/ProtectedRoute/index.jsx
+++ src/components/ProtectedRoute/index.jsx
@@ -1,19 +1,29 @@
 import { Navigate, useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
 import { useSelector } from "react-redux";
-import config from "../../config";
 
 function ProtectedRoute({ children }) {
   const location = useLocation();
   const auth = useSelector((state) => state.auth);
+
+  const publicPaths = ["/login", "/register", "/verify-phone", "/verify-card"];
 
   if (auth.loading) {
     return <div>Loading...</div>;
   }
 
   if (!auth.user) {
-    const path = encodeURIComponent(location.pathname);
-    console.log(path);
+    const isPublic = publicPaths.some((path) =>
+      location.pathname.startsWith(path)
+    );
+    if (isPublic) {
+      return children;
+    }
+    const redirectPath = encodeURIComponent(location.pathname);
+    return <Navigate to={`/login?redirect=${redirectPath}`} replace />;
+  }
+  if (auth.user && publicPaths.includes(location.pathname)) {
+    return <Navigate to="/" replace />;
   }
 
   return children;
